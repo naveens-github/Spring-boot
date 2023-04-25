2dd8dfbab2ad80d0a8e3bb568923607c3d666ee6
naveen <naveenwork8@gmail.com>
2023-04-24 21:55:25 -0500

    Spring-boot adding validations

--- Spring-boot/src/main/java/com/myproject/Springboot/entity/Department.java
+++ Spring-boot/src/main/java/com/myproject/Springboot/entity/Department.java
@@ -1,15 +1,21 @@
 package com.myproject.Springboot.entity;
 
+import org.hibernate.validator.constraints.Length;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 
 @Entity
 public class Department {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long departmentId;
+    @NotBlank(message = "Please add department name")
+    @Length(max = 10, min =2)
     private String departmentName;
     private String departmentAddress;
     private String departmentCode;
