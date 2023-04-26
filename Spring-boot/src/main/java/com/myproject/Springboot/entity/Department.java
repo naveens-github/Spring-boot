108a454afcc4c22aae1348f6458b965da4b74bba
naveen <naveenwork8@gmail.com>
2023-04-25 23:27:07 -0500

    Spring-boot adding lombok

--- Spring-boot/src/main/java/com/myproject/Springboot/entity/Department.java
+++ Spring-boot/src/main/java/com/myproject/Springboot/entity/Department.java
@@ -1,5 +1,9 @@
 package com.myproject.Springboot.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.hibernate.validator.constraints.Length;
 
 import javax.persistence.Entity;
@@ -10,7 +14,73 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Department {
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.AUTO)
+//    private long departmentId;
+//    @NotBlank(message = "Please add department name")
+//    @Length(max = 10, min =2)
+//    private String departmentName;
+//    private String departmentAddress;
+//    private String departmentCode;
+//
+//    public long getDepartmentId() {
+//        return departmentId;
+//    }
+//
+//    public void setDepartmentId(long departmentId) {
+//        this.departmentId = departmentId;
+//    }
+//
+//    public String getDepartmentName() {
+//        return departmentName;
+//    }
+//
+//    public void setDepartmentName(String departmentName) {
+//        this.departmentName = departmentName;
+//    }
+//
+//    public String getDepartmentAddress() {
+//        return departmentAddress;
+//    }
+//
+//    public void setDepartmentAddress(String departmentAddress) {
+//        this.departmentAddress = departmentAddress;
+//    }
+//
+//    public String getDepartmentCode() {
+//        return departmentCode;
+//    }
+//
+//    public void setDepartmentCode(String departmentCode) {
+//        this.departmentCode = departmentCode;
+//    }
+//
+//    public Department(long departmentId, String departmentName, String departmentAddress, String departmentCode) {
+//        this.departmentId = departmentId;
+//        this.departmentName = departmentName;
+//        this.departmentAddress = departmentAddress;
+//        this.departmentCode = departmentCode;
+//    }
+//
+//    @Override
+//    public String toString() {
+//        return "Department{" +
+//                "departmentId=" + departmentId +
+//                ", departmentName='" + departmentName + '\'' +
+//                ", departmentAddress='" + departmentAddress + '\'' +
+//                ", departmentCode='" + departmentCode + '\'' +
+//                '}';
+//    }
+//
+//    public Department (){
+//
+//    }
+
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long departmentId;
@@ -19,57 +89,4 @@ public class Department {
     private String departmentName;
     private String departmentAddress;
     private String departmentCode;
-
-    public long getDepartmentId() {
-        return departmentId;
-    }
-
-    public void setDepartmentId(long departmentId) {
-        this.departmentId = departmentId;
-    }
-
-    public String getDepartmentName() {
-        return departmentName;
-    }
-
-    public void setDepartmentName(String departmentName) {
-        this.departmentName = departmentName;
-    }
-
-    public String getDepartmentAddress() {
-        return departmentAddress;
-    }
-
-    public void setDepartmentAddress(String departmentAddress) {
-        this.departmentAddress = departmentAddress;
-    }
-
-    public String getDepartmentCode() {
-        return departmentCode;
-    }
-
-    public void setDepartmentCode(String departmentCode) {
-        this.departmentCode = departmentCode;
-    }
-
-    public Department(long departmentId, String departmentName, String departmentAddress, String departmentCode) {
-        this.departmentId = departmentId;
-        this.departmentName = departmentName;
-        this.departmentAddress = departmentAddress;
-        this.departmentCode = departmentCode;
-    }
-
-    @Override
-    public String toString() {
-        return "Department{" +
-                "departmentId=" + departmentId +
-                ", departmentName='" + departmentName + '\'' +
-                ", departmentAddress='" + departmentAddress + '\'' +
-                ", departmentCode='" + departmentCode + '\'' +
-                '}';
-    }
-
-    public Department (){
-
-    }
 }
